9c2d42ca81b0ef40f314a001245b51bf9aced1ed
Em Lazer-Walker <me@lazerwalker.com>
2019-09-02 20:48:58 -0400

    Fetch and send username

--- messages/index.js
+++ messages/index.js
@@ -1,10 +1,17 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+const fetch = require("node-fetch");
 
-const Channel = "GMZ38044W";
+const fetchUser = async function (id) {
+  const userRequest = await fetch(
+    `https://slack.com/api/users.list?token=${process.env.SlackAccessToken}`
+  ).then(response => response.json());
+  const users = userRequest.members || [];
 
-module.exports = async function(context, req) {
-  console.log(req.body);
+  return users.find(u => u.id === id);
+}
+
+module.exports = async function (context, req) {
   const payload = req.body;
   if (payload.type === "url_verification") {
     // WARNING: Here, and with all requests, we _should_ be verifying the signature of the request
@@ -24,14 +31,22 @@ module.exports = async function(context, req) {
   const event = payload.event;
   if (
     event &&
-    event.channel === Channel &&
+    event.channel === process.env.SlackChannelId &&
     event.type === "message" &&
     event.subtype !== "message_deleted"
   ) {
+
+
     let response = {
       timestamp: event.ts,
       user: event.user
     };
+
+    const user = await fetchUser(event.user)
+    if (user) {
+      response.username = user.name;
+      response.name = user.real_name;
+    }
 
     if (event.text !== "") {
       response.text = event.text;
